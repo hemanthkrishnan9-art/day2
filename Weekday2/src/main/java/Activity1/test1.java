459795a6bdcc367013de8a25a0855f81fde4c423
hemanthkrishnan9-art <hemanthkrishnan9@gmail.com>
2025-12-04 15:24:28 +0530

    Add files via upload
    week2.day2

---
+++ Weekday2/src/main/java/Activity1/test1.java
@@ -0,0 +1,15 @@
+package Activity1;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		//int
+		int ageofperson=30;
+		char logo='t';
+		String name="hemanth";
+		boolean value=true;
+		System.out.println(name);
+
+	}
+
+}
