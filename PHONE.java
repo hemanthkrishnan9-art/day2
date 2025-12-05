65dc4bd4e414a6efc37282aa8a5f9b0a05a9af90
hemanthkrishnan9-art <hemanthkrishnan9@gmail.com>
2025-12-05 10:57:38 +0530

    Add files via upload
    activity1

---
+++ PHONE.java
@@ -0,0 +1,17 @@
+package Activity1;
+
+public class PHONE {
+	
+		//methods - actions can be performed in mobile
+		//takephotos, make call
+		//Classname objectName=new ClassName();
+		public void takephoto() {
+			System.out.println("take photo");
+		}
+		
+		public static void main(String[] args) {
+			PHONE obj=new PHONE();
+			obj.takephoto();
+		}
+	}
+
